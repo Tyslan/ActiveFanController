d62562444c352e064c19fbb8b284b5b3bbeb9fe2
Lorenz Verschingel <verschingellorenz@hotmail.com>
2020-09-06 14:08:11 +0200

    Request lock for fan control

--- app/fan_controller.cpp
+++ app/fan_controller.cpp
@@ -1,21 +1,55 @@
 #include <iostream>
+#include <fstream>
 #include <thread> // std::this_thread::sleep_for
 #include <chrono> // std::chrono::seconds
 #include <syslog.h>
 #include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
 
 #include <config/inc/config.hpp>
 #include <thermal/inc/thermal.hpp>
 #include <fan/inc/fan.hpp>
 #include <pwm/inc/pwm.hpp>
+#include <file/inc/file_util.hpp>
 
 volatile sig_atomic_t stop_flag = 0;
+std ::string lock_path = "/var/lock/fanctl.lock";
 
 void set_stop_flag(int sig)
 {
    syslog(LOG_DEBUG, "Received sig %d", sig);
    stop_flag = 1;
    syslog(LOG_DEBUG, "Set stop flag to %d", stop_flag);
+}
+
+bool create_lock()
+{
+   if (exists(lock_path))
+   {
+      syslog(LOG_CRIT, "Couldn't aquire lock.");
+      return false;
+   }
+   else
+   {
+      int pid = getpid();
+      std::ofstream outfile(lock_path);
+      outfile << pid << std::endl;
+      syslog(LOG_NOTICE, "created lock %s for pid %d", lock_path.c_str(), pid);
+      return true;
+   }
+}
+
+void release_lock()
+{
+   if (remove(lock_path.c_str()) != 0)
+   {
+      syslog(LOG_ERR, "Failed to released lock on %s", lock_path.c_str());
+   }
+   else
+   {
+      syslog(LOG_NOTICE, "Succesfully released lock.");
+   }
 }
 
 int main(int argc, char *argv[])
@@ -45,7 +79,7 @@ int main(int argc, char *argv[])
    FanControl fan_control(config.get_start_fan_temp(), config.get_continious_fan_temp(), config.get_fan_jump_pwm(), config.get_upper_limit_pwm());
    PwmWriter pwm_writer(config.get_path_pwm(), config.get_fan_jump_pwm(), config.get_upper_limit_pwm());
 
-   bool running = true;
+   bool running = create_lock();
 
    while (running)
    {
@@ -64,6 +98,7 @@ int main(int argc, char *argv[])
       }
    }
 
+   release_lock();
    syslog(LOG_NOTICE, "Stopping Active Fan Control");
    closelog();
    return 0;
